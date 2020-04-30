151c9cdb95fb907f1ac77adf94a33b0942913ea9
Chris <chrishaining@yahoo.co.uk>
2020-04-30 13:02:58 +0100

    adds forms and pic handler

---
+++ puppycompanyblog/users/forms.py
@@ -0,0 +1,60 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from puppycompanyblog.models import User
+
+##################
+### Login form ###
+##################
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators = [DataRequired(), Email()])
+    password = PasswordField('Enter your password', validators = [DataRequired()])
+    submit = SubmitField('Login')
+
+#########################
+### Registration form ###
+#########################
+class RegistrationForm(FlaskForm):
+    email = StringField('Email', validators = [DataRequired(), Email()])
+
+    username = StringField('Username', validators = [DataRequired()])
+
+    password = PasswordField('Enter your password', validators = [DataRequired(), EqualTo('password_confirm', message='Passwords must match')])
+
+    password_confirm = PasswordField('Confirm your password', validators=[DataRequired()])
+
+    submit = SubmitField('Register')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('This email has already been used')
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+                raise ValidationError('This username has already been used')
+
+#######################
+### UpdateUser form ###
+#######################
+class UpdateUserForm(FlaskForm):
+    email = StringField('Email', validators = [DataRequired(), Email()])
+
+    username = StringField('Username', validators = [DataRequired()])
+
+    profile_image = FileField('Update your profile picture', validators = [ FileAllowed(['jpg', 'png'])])
+
+    password = PasswordField('Enter your password', validators = [DataRequired(), EqualTo('password_confirm', message='Passwords must match')])
+
+    password_confirm = PasswordField('Confirm your password', validators=[DataRequired()])
+
+    submit = SubmitField('Update')
+
+    def check_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError('This email has already been used')
+
+    def check_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+                raise ValidationError('This username has already been used')
---
+++ puppycompanyblog/users/picture_handler.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import os
+from flask import url_for, current_app
+
+### function to add a picture ###
+def add_profile_image(pic_upload, username):
+
+    filename = pic_upload.filename
+    ext_type = filename.split('.')[-1]
+    storage_filename = str(username)+'.'+ext_type
+
+    filepath = os.path.join(current_app.root_path,'static\profile_pics',storage_filename)
+
+    output_size = (200, 200)
+    pic = Image.open(pic_upload)
+    pic.thumbnail(output_size)
+    pic.save(filepath)
+
+    return storage_filename
